e75835542d88f2bb34adf1e3f283a9b37c3f0d53
atxmikaela <mikaela@mikaela@mikaelataylor.com>
2024-04-17 23:39:15 -0500

    almost

--- guessing-game-project/guessing-game.js
+++ guessing-game-project/guessing-game.js
@@ -5,29 +5,77 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-let secretNumber = 50;
 
-const checkGuess = (num) => {
-    if (num > secretNumber) {
-        console.log("Too high");
-        askGuess();
-    }
-    if (num < secretNumber) {
-        console.log("Too low");
-        askGuess();
-    }
-    if (num === secretNumber) {
-        console.log("You win!");
-        rl.close()
-    }
+
+function randomInRange(min, max) {
+    return Math.floor((Math.random() * (max - min) + min))
 }
+
+
+
 
 function askGuess() {
     rl.question('Enter a Guess: ', (answer) => {
         checkGuess(Number(answer))
-    }
+        return askGuess()
+    },
+        askGuess
     )
 }
 
 
-askGuess()
+
+function askRange() {
+    rl.question(
+        "Enter a max number: ",
+    (max) => {
+        let fmax = Number(max)
+        console.log(fmax)
+
+        rl.question(
+            "Enter a min number: ",
+            (min) => {
+                let fmin = Number(min)
+                console.log(fmin)
+                secretNumber = randomInRange(fmin, fmax)
+                console.log(`I'm thinking of a number between ${min} and ${max}`)
+                askGuess(secretNumber)
+            }
+        )
+    }
+)
+}
+
+let secretNumber;
+
+let counter = 5;
+
+const checkGuess = (num) => {
+    if (num > secretNumber) {
+        console.log("Too high");
+        counter--
+    if(counter === 0 ){
+        console.log("Nice try!")
+        rl.close()
+    }
+        return false
+    }
+    else if (num < secretNumber) {
+        console.log("Too low");
+        counter--
+        if(counter === 0 ){
+            console.log("Nice try!")
+            rl.close()
+        }
+            return false
+    }
+       else{
+        console.log("You win!");
+
+       }
+       return true
+
+}
+
+askRange()
+
